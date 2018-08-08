5928b8c0500ffcfbc804bd0f1cecf6297c4d521d
Andreas Boehm <andreas.boehm@documatrix.com>
2018-08-08 10:16:42 +0200

    Implemented lpr status checks
    fixed ipv6 error and improved error readability
    
    Improved error messages

--- lpr_send_test.go
+++ lpr_send_test.go
@@ -9,9 +9,7 @@ func TestDNS(t *testing.T) {
 	fmt.Println("---Start TestDNS---")
 	defer fmt.Println("--- End Test---")
 
-	var lpr LprSend
-
-	ip, err := lpr.GetIP("hq.documatrix.com")
+	ip, err := GetIP("hq.documatrix.com")
 	if err != nil {
 		fmt.Println(err.Error())
 		t.Fail()
